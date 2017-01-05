bcabdae8b7eb94a36088651056e9086200214d43
Vinicius Teixeira <vinicius0026@gmail.com>
2017-01-05 16:54:16 -0200

    bringing coverage to 100%

--- test/auth.js
+++ test/auth.js
@@ -36,13 +36,98 @@ describe('Auth', () => {
         })
       })
       .then(res => {
-        console.log('res.result', res.result)
         expect(res.statusCode).to.equal(200)
         server.stop(done)
       })
       .catch(done)
   })
+
+  it('returns 401 if wrong password is used', done => {
+    let server
+    Server.init(internals.manifest, internals.composeOptions)
+      .then(_server => {
+        server = _server
+
+        const admin = internals.User['admin']
+
+        return server.inject({
+          method: 'POST',
+          url: '/login',
+          payload: {
+            username: admin.username,
+            password: 'wrong-pass'
+          }
+        })
+      })
+      .then(res => {
+        expect(res.statusCode).to.equal(401)
+        server.stop(done)
+      })
+      .catch(done)
+  })
+
+  it('returns 401 if user doesn exist', done => {
+    let server
+    Server.init(internals.manifest, internals.composeOptions)
+      .then(_server => {
+        server = _server
+
+        return server.inject({
+          method: 'POST',
+          url: '/login',
+          payload: {
+            username: 'non-user',
+            password: 'doent matter'
+          }
+        })
+      })
+      .then(res => {
+        expect(res.statusCode).to.equal(401)
+        server.stop(done)
+      })
+      .catch(done)
+  })
+
+  it('logs user out when logged user requests GET /logout', done => {
+    let server
+    Server.init(internals.manifest, internals.composeOptions)
+      .then(_server => {
+        server = _server
+
+        const admin = internals.User['admin']
+
+        return server.inject({
+          method: 'POST',
+          url: '/login',
+          payload: {
+            username: admin.username,
+            password: admin.password
+          }
+        })
+        .then(res => {
+          const cookie = internals.getCookieFromResponse(res)
+
+          return server.inject({
+            url: '/logout',
+            headers: { cookie }
+          })
+        })
+        .then(res => {
+          expect(res.statusCode).to.equal(200)
+          server.stop(done)
+        })
+        .catch(done)
+      })
+  })
 })
+
+internals.getCookieFromResponse = res => {
+  const header = res.headers['set-cookie']
+  /* eslint-disable */
+  const cookie = header[0].match(/(?:[^\x00-\x20\(\)<>@\,;\:\\"\/\[\]\?\=\{\}\x7F]+)\s*=\s*(?:([^\x00-\x20\"\,\;\\\x7F]*))/)
+  /* eslint-enable */
+  return cookie[0]
+}
 
 internals.manifest = {
   connections: [
